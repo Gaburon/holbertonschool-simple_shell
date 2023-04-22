cfcd76f828a7a5674b751f555d3dde5671972999
Gaburon <japo000@hotmail.com>
2023-04-22 12:53:47 -0700

    Attempt 1

--- shell.c
+++ shell.c
@@ -1,29 +1,86 @@
-##include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "shell.h"
 
-#define BUFFER_SIZE 1024
+/**
+ * execute - Execute function for simple_shell
+ * @argv: Argument vector
+ * Return: 0 on succes execution, -1 on exit
+ */
 
-int main(void)
+int execute(char **argv)
 {
-	char buffer[BUFFER_SIZE];
-	size_t length;
-	int error_code;
+	if (_strcmp(argv[0], "env") == 0)
+	{
+		env_builtin();
+		return (0);
+	}
+	execve(argv[0], argv, environ);
+	return (0);
+}
+/**
+ * env_builtin - prints the environment
+ */
 
-	while (1) {
-		printf("$ "); /* Prompt */
-		length = read(STDIN_FILENO, buffer, sizeof(buffer));
+void env_builtin(void)
+{
+	int i;
 
-		if (length == 0) {
-			exit(EXIT_SUCCESS);
-		}
-
-		if (buffer[length - 1] == '\n') {
-			buffer[length - 1] = '\0';
-		}
-
-		error_code = execve(buffer, (char *const[]) { buffer, NULL }, NULL);
-		if (error_code == -1) {
-			perror("Error executing command");
+	if (environ)
+	{
+		for (i = 0; environ[i]; i++)
+		{
+			write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+			write(STDOUT_FILENO, "\n", 1);
 		}
 	}
+}
+/**
+ * init_shell - Fuction to execute simple shell
+ * @prompt: Prompt to print
+ * @sh_command: command used to execute the shell
+ * @aux: Auxiliar size_t to getline
+ * Return: 0 on success execution, 1 on malloc failure
+ */
+
+int init_shell(char *prompt, char *sh_command, size_t aux)
+{
+	char **argv, *errorline, *line = NULL;
+	pid_t child_pid;
+	ssize_t linelen = 0;
+	int status, argvst, argc = 0;
+
+	while (1)
+	{
+		ISATTYPROMPT(prompt, _strlen(prompt));
+		line = _calloc(100, sizeof(*line));
+		if (line == NULL)
+			return (1);
+		linelen = getline(&line, &aux, stdin);
+		argc = count_words(line);
+		if (linelen == 1 || argc == linelen - 1)
+		{
+			free(line);
+			continue;
+		}
+		if (linelen == -1 || _strcmp(line, "exit\n") == 0)
+			break;
+		argv = _calloc((argc + 2), sizeof(*argv));
+		if (argv == NULL)
+			break;
+		argvst = linetoargv(line, argv, linelen);
+		errorline = get_errorline(sh_command, argv[0]);
+		if (argvst >= 0)
+		{
+			child_pid = fork();
+			if (child_pid == 0)
+				execute(argv);
+			else
+				wait(&status);
+			FREELAR(line, argvst, argv[0], argv, errorline);
+		}
+		else
+			FREEWRITE(line, argv, errorline);
+		ISATTYOUT;
+	}
+	free(line);
+	return (0);
+}
