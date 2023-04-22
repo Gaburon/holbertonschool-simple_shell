554f9b3d586194808c0a5681fcad7d7c56eba012
Gaburon <japo000@hotmail.com>
2023-04-22 12:48:27 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -1,26 +1,29 @@
+##include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
 int main(void)
 {
-	char buffer[1024];
-	int error_code;
+	char buffer[BUFFER_SIZE];
 	size_t length;
+	int error_code;
 
 	while (1) {
 		printf("$ "); /* Prompt */
 		length = read(STDIN_FILENO, buffer, sizeof(buffer));
-		if (length == -1) {
-			perror("read");
-			return (EXIT_FAILURE);
-		}
+
 		if (length == 0) {
-			printf("\n");
-			return (EXIT_SUCCESS);
+			exit(EXIT_SUCCESS);
 		}
-		buffer[length - 1] = '\0';
-		char *args[] = { buffer, NULL };
-		error_code = execve(buffer, args, NULL);
+
+		if (buffer[length - 1] == '\n') {
+			buffer[length - 1] = '\0';
+		}
+
+		error_code = execve(buffer, (char *const[]) { buffer, NULL }, NULL);
 		if (error_code == -1) {
-			perror("execve");
-			return (EXIT_FAILURE);
+			perror("Error executing command");
 		}
 	}
-}
