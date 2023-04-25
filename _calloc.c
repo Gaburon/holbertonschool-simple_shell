8cbdca55549837d54c7892c358ed0738d87a1702
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-04-25 13:09:10 -0500

    Update _calloc.c

--- _calloc.c
+++ _calloc.c
@@ -2,6 +2,8 @@
 
 /**
  * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: Amount of elements of the array
+ * @size: Size of each element
  * Return: pointer to the allocated memory, or NULL in fail
  */
 
