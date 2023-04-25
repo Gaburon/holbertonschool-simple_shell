744ea0f4c57e9d6dddfb7e1dec050bab36644927
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-04-25 13:03:19 -0500

    Calloc

--- _calloc.c
+++ _calloc.c
@@ -9,15 +9,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
-	char *ret;
+    char *ret;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	ret = malloc(nmemb * size);
-	if (ret == NULL)
-		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
-		ret[i] = 0x00;
-	return (ret);
+    if (nmemb == 0 || size == 0)
+        return NULL;
+
+    ret = malloc(nmemb * size);
+    if (ret == NULL)
+        return NULL;
+
+    memset(ret, 0, nmemb * size);
+
+    return ret;
 }
