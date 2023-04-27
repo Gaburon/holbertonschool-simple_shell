6ee0c69a951ff9ac67af59558c506dc03f89534e
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-04-27 15:22:19 -0500

    Update init_shell.c

--- init_shell.c
+++ init_shell.c
@@ -6,15 +6,43 @@
  * Return: 0 on succes execution, -1 on exit
  */
 
-int execute(char **argv)
+void execute(char **args)
 {
-	if (_strcmp(argv[0], "env") == 0)
+	pid_t pid;
+	int status;
+	if(args[0] != NULL)
+		pid = fork();
+	/**
+         *if pid = 0, this is the child proccess
+         */
+	if (pid == 0)
 	{
-		env_builtin();
-		return (0);
+		if (strcmp(args[0], "env") == 0)
+			builtin_env();
+		/**
+		    Child process */
+		if (execve(args[0], args, environ) == -1)
+		{
+		    perror("execute");
+			exit(EXIT_FAILURE);
+		}
+
+		else if (pid < 0)
+		{
+			/**
+			   Error forking */
+			perror("fork");
+		}
+		else
+		{
+			/**
+			   Parent process */
+                do
+		{
+		wait(&status);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		}
 	}
-	execve(argv[0], argv, environ);
-	return (0);
 }
 
 /**
